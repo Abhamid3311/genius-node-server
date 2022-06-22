0b7028ba74d0918ff473b8329c21112b4f78ca3c
Hamid <www.abdulhamid3311@gmail.com>
2022-06-22 12:54:55 +0600

    added JWT

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ require('dotenv').config()
 const cors = require('cors');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const res = require('express/lib/response');
+var jwt = require('jsonwebtoken');
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -10,6 +11,23 @@ const port = process.env.PORT || 5000;
 //use Middleware
 app.use(cors());
 app.use(express.json());
+
+
+function verifyJWT(req, res, next) {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        return res.status(401).send({ message: 'UnAuthorized access' });
+    };
+    const token = authHeader.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+            return res.status(403).send({ message: 'forbidden access' });
+        }
+        console.log('Decoded', decoded);
+        req.decoded = decoded;
+        next();
+    });
+};
 
 
 
@@ -20,7 +38,18 @@ async function run() {
     try {
         await client.connect();
         const serviceCollection = client.db("geniusCar").collection("service");
+        const orderCollection = client.db("geniusCar").collection("order");
 
+        //Post token From login page
+        app.post('/login', async (req, res) => {
+            const user = req.body;
+            const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+                expiresIn: '1d'
+            });
+            res.send({ accessToken });
+        });
+
+        //SERVICES API
         app.get('/service', async (req, res) => {
             const query = {};
             const cursor = serviceCollection.find(query);
@@ -33,14 +62,14 @@ async function run() {
             const service = await serviceCollection.findOne(query);
             console.log(service);
             res.send(service);
-        })
+        });
 
-        //POST
+        //Service POST
         app.post('/service', async (req, res) => {
             const newService = req.body;
             const result = await serviceCollection.insertOne(newService);
             res.send(result);
-        })
+        });
 
         //DELETE
         app.delete('/service/:id', async (req, res) => {
@@ -48,7 +77,28 @@ async function run() {
             const query = { _id: ObjectId(id) };
             const deleteItem = await serviceCollection.deleteOne(query);
             res.send(deleteItem);
-        })
+        });
+
+        //Order Get
+        app.get('/order', verifyJWT, async (req, res) => {
+            const decodedEmail = req.decoded.email;
+            const email = req.query.email;
+            if (email === decodedEmail) {
+                const query = { email: email };
+                const cursor = orderCollection.find(query);
+                const orders = await cursor.toArray();
+                res.send(orders);
+            } else {
+                return res.status(403).send({ message: 'forbidden access' });
+            }
+        });
+        //Order Post
+        app.post('/order', async (req, res) => {
+            const order = req.body;
+            const result = await orderCollection.insertOne(order);
+            res.send(result);
+            ;
+        });
 
     } finally {
 
